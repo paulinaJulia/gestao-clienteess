3699ff7055d7d400ed5111eb811049071f2f3ef7
paulinaJulia <paulinajulia2015@gmail.com>
2021-10-14 11:47:26 -0300

    modificacao

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+
 ALLOWED_HOSTS = ['gestao-clienteess.herokuapp.com/']
 
 
